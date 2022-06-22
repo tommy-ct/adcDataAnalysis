c62b3feadaff74832fc81583f64bb48b48be9389
tommy-ct <ct112358@outlook.com>
2022-06-22 20:21:55 +0800

    add comment

--- analysis.py
+++ analysis.py
@@ -8,4 +8,9 @@ import matplotlib.pyplot as plt
 #导入用户自定义库
 
 def data_analysis(data = [0], fs = 1, FFTtime = 1, bitnum = 10, M = 1, nyquist_band = 1):
+    '''
+    Input:
+
+    Output:
+    '''
     pass
